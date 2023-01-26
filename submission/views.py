63878d035f1d14689b9ca29718ef8301578d7e1c
Samad-sust <masamad.sust@gmail.com>
2023-01-26 23:42:23 +0600

    base templating added, listview created

--- submission/views.py
+++ submission/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Submission
 
 # Create your views here.
+
+class SubmissionListView(ListView):
+    model = Submission
+    template_name = "submission_list.html"
