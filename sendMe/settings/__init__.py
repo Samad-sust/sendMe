bbfb22ebec1caf045d560496ce8805db2d4744f8
samad-sust <masamad.sust@gmail.com>
2023-01-31 14:38:02 +0600

    setup config for production settings

--- sendMe/settings/__init__.py
+++ sendMe/settings/__init__.py
@@ -1,10 +1,8 @@
 from .base import *
 # you need to set "myproject = 'prod'" as an environment variable
 # in your OS (on which your website is hosted)
-print('Working fine init')
-# if os.environ['myproject'] == 'prod':
-myproject = 'dev'
-if myproject == 'prod':
+app_mode = os.environ.get('SEND_ME_DEVELOPMENT_MODE')
+if app_mode == 'prod':
    from .prod import *
 else:
    from .dev import *
--- sendMe/settings/prod.py
+++ sendMe/settings/prod.py
@@ -1,1 +1,31 @@
-print('Prod.py')
+from pathlib import Path
+import os
+
+DEBUG = True
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+print(BASE_DIR)
+ALLOWED_HOSTS = [
+    '192.168.31.242',
+    '192.168.1.103',
+    '192.168.1.1',
+    '127.0.0.1',
+]
+
+# postgre database configuration
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'sendme',
+    'USER': 'sendme',
+    'PASSWORD': '123@$456',
+    'HOST': '127.0.0.1',
+    'PORT': '5432',
+    }
+}
+
+print('Dev.py')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
