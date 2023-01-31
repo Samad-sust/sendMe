2c92a22f30307456d6abfa2bfe1895a38f2b305c
samad-sust <masamad.sust@gmail.com>
2023-01-31 14:43:42 +0600

    update production setting to debug false

--- sendMe/settings/prod.py
+++ sendMe/settings/prod.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-DEBUG = True
+DEBUG = False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
